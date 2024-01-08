b158f9292f09c9e3e69ab4a417b260a112b1b62f
highb33kay <hiibeekayvibe@gmail.com>
2024-01-08 01:28:45 +0100

    Init

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,21 @@
+import express, { Router } from "express";
+import { login, register } from "../controllers/auth.controller";
+
+const router: Router = express.Router();
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+// router.delete("/logout", function (req, res, next) {
+//   if (req.session) {
+//     req.session.destroy((err) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       return res.redirect("https://evento1.vercel.app");
+//     });
+//   }
+// });
+
+module.exports = router;
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from "./responsehandler";
+export * from "./prisma";
